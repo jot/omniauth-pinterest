7616df6cb4ddf0e765c2d148885f806dbc772e59
Raison Dsouza <raisondsouza@gmail.com>
2012-07-11 16:19:46 +0530

    few specs added

--- spec/omniauth/strategies/pinterest_spec.rb
+++ spec/omniauth/strategies/pinterest_spec.rb
@@ -1,7 +1,22 @@
 require 'spec_helper'
 
 describe OmniAuth::Strategies::Pinterest do
-  it 'should have tests' do
-    pending
+
+  context "client options" do
+    subject do
+      OmniAuth::Strategies::Pinterest.new({})
+    end
+
+    it 'should have correct site' do
+      subject.options.client_options.site.should eq('https://pinterest.com')
+    end
+
+    it 'should have correct authorize url' do
+      subject.options.client_options.authorize_url.should eq('https://pinterest.com/oauth/authorize')
+    end
+
+    it 'should have correct token url' do
+      subject.options.client_options.token_url.should eq('https://api.pinterest.com/v2/oauth/access_token')
+    end
   end
 end
