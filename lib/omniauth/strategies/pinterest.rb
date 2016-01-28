a97d723bb7544e1909f3541068b9ae01349c7fa0
Ryan Angilly <ryan@ramen.is>
2016-01-28 13:33:23 -0700

    Allow modifying redirect_uri on the fly

--- lib/omniauth/strategies/pinterest.rb
+++ lib/omniauth/strategies/pinterest.rb
@@ -18,6 +18,14 @@ module OmniAuth
       uid { raw_info['id'] }
 
       info { raw_info }
+      
+      def authorize_params
+        super.tap do |params|
+          %w[redirect_uri].each do |v| 
+            params[:redirect_uri] = request.params[v] if request.params[v]
+          end 
+        end 
+      end 
 
       def raw_info
         @raw_info ||= access_token.get('/v1/me/').parsed['data']
